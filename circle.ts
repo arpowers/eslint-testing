df7b3a0877c291a2ed897161c65f041e8ee9fe0e
Andrew Powers <arpowers@gmail.com>
2023-12-23 09:23:37 -0700

    structure

---
+++ circle.ts
@@ -0,0 +1,9 @@
+import { functionTwo } from '.'
+
+export function functionOne() {
+  return true
+}
+
+export function whatever2() {
+  return functionTwo()
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-async function testing() {
-  console.log("testing");
+import { functionOne } from './circle'
+
+export function functionTwo() {
+  return true
 }
 
-testing();
+export function whatever() {
+  return functionOne()
+}
+
+async function floatingPromise() {
+  console.log('floatingPromise')
+}
+
+floatingPromise()
